4923d16c006427124f729c3c4e9b632c195627b6
hacker-flavio <theflavioherrera@gmail.com>
2023-12-26 17:30:33 -0800

    added search function

--- projects/numberOfPets/CreateArrayList.h
+++ projects/numberOfPets/CreateArrayList.h
@@ -7,6 +7,28 @@
 #include <stdexcept>
 #include <string>
 
+struct Success {
+    bool state;
+    int index;
+    std::string response;
+    Success() {
+        state = false;
+        index = 0;
+        response = "not found";
+    }
+
+    Success(bool state, std::string response) {
+        this->state = state;
+        this->response = response;
+    }
+
+    Success(bool state, int index, std::string response) {
+        this->state = state;
+        this->index = index;
+        this->response = response;
+    }
+};
+
 struct Pet {
     std::string name;
     int age;
@@ -14,11 +36,29 @@ struct Pet {
         name = "pet-name";
         age = 0;
     }
+    Pet(std::string name) { this->name = name; }
     Pet(std::string name, int age) {
         this->name = name;
         this->age = age;
     }
+
+    // Define equality operator
+    bool operator==(const Pet& other) const {
+        // search by both name and age
+        //  return name == other.name && age == other.age;
+
+        // only search by nane
+        return name == other.name;
+    }
 };
+
+// overloaded operator <<
+std::ostream& operator<<(std::ostream& os, const Pet& myPet) {
+    os << "( name: " << myPet.name << ", "
+       << "age: " << myPet.age << " )";
+
+    return os;
+}
 
 class CreateArrayList {
    private:
@@ -27,6 +67,12 @@ class CreateArrayList {
 
     // default constructor
     CreateArrayList() {}
+
+    // accepts
+    CreateArrayList(std::string name, int age) {
+        Pet pet(name, age);
+        myArray.append(pet);
+    }
 
     // copy constructor
     CreateArrayList(const CreateArrayList& other) {
@@ -41,6 +87,13 @@ class CreateArrayList {
             myArray.append(other.myArray[i]);
         }
         return *this;
+    }
+
+    Pet& operator[](int index) const {
+        if (index < 0 || index >= myArray.size()) {
+            throw std::logic_error("Array index out of bounds.");
+        }
+        return myArray[index];
     }
 
     // add data from file
@@ -70,6 +123,25 @@ class CreateArrayList {
         Pet newPet(name, age);
         myArray.append(newPet);
     }
+
+    // remove first item
+    Pet removeFirst() { return myArray.removeFirst(); }
+    Pet removeLast() { return myArray.removeLast(); }
+
+    bool search(Pet myPet) { return myArray.search(myPet); }
+
+    // find object
+    Success find(std::string name) {
+        for (int i = 0; i < myArray.size(); i++) {
+            if (myArray[i].name == name) {
+                std::string response = "found: " + name;
+                Success message(true, i, response);
+                return message;
+            }
+        }
+        Success message;
+        return message;
+    }
 };
 
 // overloaded operator <<
@@ -79,6 +151,7 @@ std::ostream& operator<<(std::ostream& os, const CreateArrayList& myList) {
         os << "name: " << pet.name << ", "
            << "age: " << pet.age << std::endl;
     }
+    os << "total number of entries -> " << myList.myArray.size() << std::endl;
 
     return os;
 }
--- projects/numberOfPets/main.cpp
+++ projects/numberOfPets/main.cpp
@@ -20,12 +20,48 @@ int main() {
     cats.addData("cat_pets.csv");
     CreateArrayList old_cats_list;
     old_cats_list = cats;
+    cout << "removing -> " << cats.removeFirst() << endl;
+    cout << "removing -> " << cats.removeLast() << endl;
     cats.append("pussy", 10);
+
+    CreateArrayList birds("chirpy", 4);
+    birds.append("flappy", 3);
 
     cout << dogs << endl;
     cout << old_dogs_list << endl;
     cout << cats << endl;
     cout << old_cats_list << endl;
+
+    // Example 1
+    string pet_name = "Tiger";
+    Success output;
+    output = cats.find(pet_name);
+    cout << output.response << endl;
+
+    // Example 2
+    string pet_name2 = "furry";
+    Success output2(cats.find(pet_name2));
+    cout << output2.response << endl;
+
+    // Example 3
+    Success output3(cats.find("destroyer9000"));
+    cout << output3.response << endl;
+
+    // Example 4
+    std::cout << Success(cats.find("Oreo")).response << std::endl;
+
+    // search by both name and age
+    //  Pet tempPet("Cleo", 3);
+
+    // search only by name
+    Pet tempPet("Cleo");
+
+    string successResponse = "Pet: " + tempPet.name + " found in cats";
+    string response = cats.search(tempPet) ? successResponse : "not found";
+    cout << Success(cats.search(tempPet), response).response << endl;
+
+    int index = 0;
+    cout << "bird number[" << index << "]: " << birds[index] << endl;
 
     return 0;
 }
